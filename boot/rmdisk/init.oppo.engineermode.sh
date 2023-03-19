7c46395951b12ce4f4b9b036b6e2420cfbc7c914
justhuman6 <96756585+justhuman6@users.noreply.github.com>
2023-03-19 05:54:54 -0700

    Add files via upload

---
+++ boot/rmdisk/init.oppo.engineermode.sh
@@ -0,0 +1,16 @@
+#!/system/bin/sh
+
+##################################################################
+if [ -f /persist/engineermode/adb_switch ]; then
+    setprop persist.sys.allcommode true
+    setprop persist.allcommode true
+    setprop persist.sys.oppo.usbactive true
+    setprop persist.sys.adb.engineermode 0
+    setprop sys.usb.config mtp,adb
+    setprop persist.sys.usb.config mtp,adb
+    adb_switch=`cat /persist/engineermode/adb_switch`
+    if [ "$adb_switch"x = "ENABLE_BY_MASTERCLEAR"x ]; then
+        rm /persist/engineermode/adb_switch
+    fi
+fi
+###################################################################
